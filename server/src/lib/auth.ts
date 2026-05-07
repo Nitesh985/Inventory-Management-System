1c0b1ae18a82e252d0af8bb4b896251a04a47833
Nitesh985 <niteshacharya759@gmail.com>
2026-05-07 08:05:08 +0545

    updated better auth issues

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -49,4 +49,13 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string || "",
     }
   },
+  advanced:{
+    crossSubdomainCookies:{
+      enabled: true,
+    },
+    defaultCookieAttributes:{
+      sameSite: "none",
+      secure: true
+    }
+  }
 });
